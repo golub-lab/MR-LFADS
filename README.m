4f26f9829053f4051888d4d3c3ab6c24be4004eb
Belle Liu <belle.l24358@gmail.com>
2026-01-08 13:47:49 -0800

    Update README

--- README.m
+++ README.m
@@ -1,1 +1,60 @@
-MR-LFADS
+<!-- PROJECT SHIELDS -->
+<p align="center">
+  [![Contributors][contributors-shield]][contributors-url]
+  [![Forks][forks-shield]][forks-url]
+  [![Stargazers][stars-shield]][stars-url]
+  [![Issues][issues-shield]][issues-url]
+  [![MIT License][license-shield]][license-url]
+</p>
+
+<br />
+
+<p align="center">
+  <h3 align="center">
+    Multi-Regional Latent Factors via Dynamical Systems Analysis (MR-LFADS)
+  </h3>
+</p>
+
+<!-- TABLE OF CONTENTS -->
+<details open="open">
+  <summary>Table of Contents</summary>
+  <ol>
+    <li><a href="#about-the-project">About The Project</a></li>
+    <li><a href="#getting-started">Getting Started</a></li>
+    <li><a href="#tutorials">Tutorials</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li>
+  </ol>
+</details>
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+[![Product Name Screen Shot][product-screenshot]](https://example.com)
+
+Use this section to describe the motivation, scope, and scientific contributions of MR-LFADS.
+
+You may include:
+- Problem formulation and objectives  
+- Methodological overview  
+- Intended applications and datasets  
+
+### Built With
+
+List major frameworks, libraries, and dependencies.
+
+- Example dependency 1  
+- Example dependency 2  
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+Instructions for setting up the project locally.
+
+### Prerequisites
+
+List required software and dependencies.
+
+```sh
+# Example
+npm install npm@latest -g
