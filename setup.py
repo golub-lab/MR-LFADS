f9250b3ba1a52531aba7480d4e926b278399d387
Belle Liu <belle.l24358@gmail.com>
2026-01-08 20:23:04 -0800

    Add setup, requireiments, gitignore

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="mrlfads",
+    author="Belle Liu",
+    author_email="belle.l24358@gmail.com",
+)
+
+setup(
+    name="dgn",
+    author="Belle Liu",
+    author_email="belle.l24358@gmail.com",
+)
+
+setup(
+    name="tools",
+    author="Belle Liu",
+    author_email="belle.l24358@gmail.com",
+)
